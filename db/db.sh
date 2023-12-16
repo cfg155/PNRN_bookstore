a1ba7b1660da38398b9c92bf47f6928b82e47a2d
Louis Leonardo <leonardolouis2@gmail.com>
2023-12-16 11:35:42 +0700

    init db installation

---
+++ db/db.sh
@@ -0,0 +1,9 @@
+apt update && apt install -y
+apt-get install -y
+
+# Install Postgres
+apt install postgresql -y
+
+# append config to postgres file
+echo "host  all     all     0.0.0.0/0   trust" >> /etc/postgresql/12/main/pg_hba.conf
+echo "listen_addresses = '*'" >> /etc/postgresql/12/main/postgresql.conf"
