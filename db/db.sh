afd74793e07f7bdc6e5397777cab594fb3ff7f97
Louis Leonardo <leonardolouis2@gmail.com>
2023-12-19 00:00:02 +0700

    update db sh

--- db/db.sh
+++ db/db.sh
@@ -7,3 +7,5 @@ apt install postgresql -y
 # append config to postgres file
 echo "host  all     all     0.0.0.0/0   trust" >> /etc/postgresql/12/main/pg_hba.conf
 echo "listen_addresses = '*'" >> /etc/postgresql/12/main/postgresql.conf
+
+systemctl restart postgresql
